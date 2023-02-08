c7e7fdc5aae6c1d7112b904243742ee4ccd2596c
Shubham2157 <1830193@kiit.ac.in>
2023-02-08 20:42:44 +0530

    download log file issue fix

--- index.js
+++ index.js
@@ -32,19 +32,23 @@ app.get('/', (req,res)=>{
 //                 LOGS
 
 app.get("/download/logs/application.log", (req,res) => {
-  var fs = require("fs");
-  fs.readFile('./prodlogs/app.log', function (err, data) {
-    res.send(data)
- });
+  const file = `${__dirname}/prodlogs/app.log`
+  var d = new Date()
+  var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" +
+    d.getFullYear() + "_" + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2)+ ":" + ("0" + d.getSeconds()).slice(-2);
+  const filename = `application-${datestring}.log`
+  res.download(file, filename)
 })
 
 
 
 app.get("/download/logs/error.log", (req,res) => {
-  var fs = require("fs");
-  fs.readFile('./prodlogs/error.log', function (err, data) {
-    res.send(data)
- });
+  const file = `${__dirname}/prodlogs/error.log`
+  var d = new Date()
+  var datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" +
+    d.getFullYear() + "_" + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2)+ ":" + ("0" + d.getSeconds()).slice(-2);
+  const filename = `error-logs-${datestring}.log`
+  res.download(file, filename)
 })
 
 // ===================================================================
